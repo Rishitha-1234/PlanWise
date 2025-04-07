ab1093f1d1cf692989e45c744eda47457da2e6c3
Rishitha Singalreddy <heytheremysekfrishithareddy@gmail.com>
2025-04-07 07:23:17 +0530

    deployment changes fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+const root = createRoot(document.getElementById("root")!);
 
-
-
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
